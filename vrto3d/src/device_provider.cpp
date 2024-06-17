73fec809ff24352183dec10ed5792c26abe70110
OneUp <oneup03@gmail.com>
2024-06-17 18:55:45 -0500

    Licensing update

--- vrto3d/src/device_provider.cpp
+++ vrto3d/src/device_provider.cpp
@@ -1,4 +1,19 @@
-//============ Copyright (c) Valve Corporation, All rights reserved. ============
+/*
+ * This file is part of VRto3D.
+ *
+ * VRto3D is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VRto3D is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with <Your Project Name>. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "device_provider.h"
 
 #include "driverlog.h"
--- vrto3d/src/key_mappings.h
+++ vrto3d/src/key_mappings.h
@@ -1,3 +1,19 @@
+/*
+ * This file is part of VRto3D.
+ *
+ * VRto3D is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VRto3D is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with <Your Project Name>. If not, see <http://www.gnu.org/licenses/>.
+ */
 #pragma once
 #ifndef VIRTUAL_KEY_MAPPINGS_H
 #define VIRTUAL_KEY_MAPPINGS_H
