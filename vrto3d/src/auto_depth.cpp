4cc04398298d0a2a944931befb64d43c55da9eb5
OneUp <oneup03@gmail.com>
2026-05-02 19:08:11 -0500

    Cleanup logging

--- vrto3d/src/auto_depth.cpp
+++ vrto3d/src/auto_depth.cpp
@@ -23,7 +23,6 @@
 
 #include <algorithm>
 #include <cstring>
-#include <sstream>
 
 #include <d3dcompiler.h>
 
@@ -267,7 +266,6 @@ bool AutoDepthAnalyzer::EnsureResources(ID3D11Texture2D* sbs)
         }
     }
 
-    LOG() << "AutoDepth: resources ready";
     return sbs_srv_ != nullptr;
 }
 
@@ -375,25 +373,6 @@ void AutoDepthAnalyzer::Run(ID3D11DeviceContext*    ctx,
             }
 
             component->FeedAutoDepthSample(max_disp, eye_w);
-
-            if (frame_counter <= 5 || (frame_counter % 120 == 0)) {
-                LOG() << "AutoDepth: max_disp=" << max_disp
-                      << " px (eye_w=" << eye_w
-                      << ", search=" << search_radius
-                      << ", bucket=" << top_bucket
-                      << ", frac=" << (eye_w ? float(max_disp) / float(eye_w) : 0.0f)
-                      << ", matches=" << match_count
-                      << ")";
-                std::ostringstream ss;
-                ss << "AutoDepth hist:";
-                for (int b = 0; b < NUM_BUCKETS - 8; b += 4) {
-                    ss << " [" << b << "]=" << hist[b];
-                }
-                for (int b = NUM_BUCKETS - 8; b < NUM_BUCKETS; ++b) {
-                    ss << " [" << b << "]=" << hist[b];
-                }
-                LOG() << ss.str();
-            }
         }
         // DXGI_ERROR_WAS_STILL_DRAWING -> just try again next frame.
     }
